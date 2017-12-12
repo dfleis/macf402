4ec4fb10a7833f335d70f2ee3ec0823fba0358ee
dfleis <david.p.fleischer@gmail.com>
2017-12-11 21:05:39 -0500

    ftsc variation over J

--- ftcs.R
+++ ftcs.R
@@ -13,78 +13,67 @@
 # u(x,t) = e^(-t)sin(x)
 #
 # implement the FTCS method and compare to the analytic (true) solution
-
 xL <- 0
 xR <- pi
-tmax <- 3
 T <- 3
 
 
 N <- 476
 J <- 28
 
-dx <- (xR - xL)/J
 dt <- T/N
-
+dx <- (xR - xL)/J
 v <- dt/(dx^2)
 
 # We define U on the interior of the space domain
-
-Uint <- matrix(0,nrow = (J-1),ncol = (N+1))
-
+Uint <- matrix(0, nrow = (J - 1), ncol = (N + 1))
 
 # We add the initial condition at time t=0
-for (i in 1:(J-1)){
-    Uint[i,1] <- sin(i*dx)
+for (i in 1:(J - 1)) {
+    Uint[i, 1] <- sin(i*dx)
 }
 
-
 aa <- v
-bb <- 1-2*v
+bb <- 1 - 2*v
 cc <- v
-
-J2 <- J-1
+J2 <- J - 1
 
 # Create the tridiagonal matrix tridiag(aa,bb,cc)
 # Note that the A here is the (I-vA) in the notes
-
-A <- matrix(nrow=J2,ncol=J2,data=0)
+A <- matrix(nrow = J2, ncol = J2, data = 0)
 diag(A) <- bb
-A[cbind(1:(J2-1),2:J2)] <- cc
-A[cbind(2:J2,1:(J2-1))] <- aa
+A[cbind(1:(J2 - 1), 2:J2)] <- cc
+A[cbind(2:J2, 1:(J2 - 1))] <- aa
 
 ## Apply the FTCS
 for (n in 1:N){
-    Uint[,n+1] <- A%*%Uint[,n]
+    Uint[,n + 1] <- A %*% Uint[,n]
 }
 
 # We add the boundary conditions at x_L and x_R
-C <- rbind(0,Uint,0)
+C <- rbind(0, Uint, 0)
 
 # Plot the approximate solution
-t <- seq(0,T,by=dt)
-x <- seq(xL,xR,by=dx)
-persp(x,t,C,theta=45,phi=15)
+t <- seq(0, T, by = dt)
+x <- seq(xL, xR, by = dx)
+persp(x, t, C, theta = 45, phi = 15)
 
 # Construct the true solution
 N <- length(t)
 J <- length(x)
-
-C_true = matrix(nrow=J,ncol=N,data=0)
+C_true <- matrix(nrow = J, ncol = N, data = 0)
 
 for (n in 1:N){
     for (j in 1:J){
-    	C_true[j,n] <- exp(-t[n])*sin(x[j])
+    	C_true[j, n] <- exp(-t[n])*sin(x[j])
 	}
 }
 
 # Compare the approximate solution C from FTCS and the true solution C_true 
-
-persp(x,t,C-C_true,theta=45,phi=15)
+persp(x, t, C - C_true, theta = 45, phi = 15)
 
 # Calculate the maximum error
-
-max(abs(C-C_true))
+max(abs(C - C_true))
 
 ## For J=14 and N=199 we find the maximum error is
 ## 0.0012344
@@ -95,5 +84,4 @@ max(abs(C-C_true))
 ## with J=28 this means we must have N >= 476.6148
 ## If we take J=28 and N=477 then we find that the maximum error is 
 ## 0.0007727066
-
 
---
+++ ftcs_over_J.R
@@ -0,0 +1,80 @@
+# FTCS Example for canonical heat equation
+# u_t = u_xx
+# u(0,x) = lambda(x)
+# u(t,x_L) = alpha(t)
+# u(t,x_R) = beta(t)
+#
+# where we assume that x_L = 0, x_R = pi
+# lambda(x) = sin(x)
+# alpha(t) = 0, beta(t) = 0
+#
+#
+# can show analytically that the solution to the above problem is
+# u(x,t) = e^(-t)sin(x)
+#
+# implement the FTCS method and compare to the analytic (true) solution
+xL <- 0
+xR <- pi
+T <- 3
+
+N <- 200
+Jmin <- 15
+Jmax <- 25
+Js <- Jmin:Jmax
+errvec_J <- vector(mode = 'numeric', length = Jmax - Jmin + 1)
+
+for (J in Js) {
+  
+  dt <- T/N
+  dx <- (xR - xL)/J
+  v <- dt/(dx^2)
+  
+  # We define U on the interior of the space domain
+  Uint <- matrix(0, nrow = (J - 1), ncol = (N + 1))
+  
+  # We add the initial condition at time t=0
+  for (i in 1:(J - 1)) {
+    Uint[i, 1] <- sin(i*dx)
+  }
+  
+  aa <- v
+  bb <- 1 - 2*v
+  cc <- v
+  J2 <- J - 1
+  
+  # Create the tridiagonal matrix tridiag(aa,bb,cc)
+  # Note that the A here is the (I-vA) in the notes
+  A <- matrix(nrow = J2, ncol = J2, data = 0)
+  diag(A) <- bb
+  A[cbind(1:(J2 - 1), 2:J2)] <- cc
+  A[cbind(2:J2, 1:(J2 - 1))] <- aa
+  
+  ## Apply the FTCS
+  for (n in 1:N){
+    Uint[,n + 1] <- A %*% Uint[,n]
+  }
+  
+  # We add the boundary conditions at x_L and x_R
+  C <- rbind(0, Uint, 0)
+  
+  # Plot the approximate solution
+  t <- seq(0, T, by = dt)
+  x <- seq(xL, xR, by = dx)
+  
+  # Construct the true solution
+  N <- length(t)
+  J3 <- length(x)
+  C_true <- matrix(nrow = J3, ncol = N, data = 0)
+  
+  for (n in 1:N){
+    for (j in 1:J){
+      C_true[j, n] <- exp(-t[n])*sin(x[j])
+    }
+  }
+  
+  # Calculate the maximum error
+  errvec_J[J - Jmin + 1] <- max(abs(C - C_true))
+}
+
+plot(errvec_J ~ Js, type = 'o', pch = 19)
+plot(log10(errvec_J) ~ Js, type = 'o', pch = 19)
